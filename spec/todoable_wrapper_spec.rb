546538507d9d53e1f82c45cf60ebbed1a28bdd59
Sam Hoffman <samdhoffman@gmail.com>
2020-02-10 10:24:25 -0500

    Update todoable wrapper spec

--- spec/todoable_wrapper_spec.rb
+++ spec/todoable_wrapper_spec.rb
@@ -2,8 +2,5 @@ RSpec.describe TodoableWrapper do
   it "has a version number" do
     expect(TodoableWrapper::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  
 end
