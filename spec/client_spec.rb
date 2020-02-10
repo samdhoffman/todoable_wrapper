c9f37ff9b6828fe3e2b8a1bcb07aedd9ef0b4496
Sam Hoffman <samdhoffman@gmail.com>
2020-02-10 10:26:11 -0500

    Add tests and fixture for lists get request

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -34,5 +34,34 @@ RSpec.describe TodoableWrapper::Client do
       expect(client.instance_variable_get(:@token)).to eq("06e4d7f1-392c-4b8a-acad-d111ee615729")
     end
   end
+
+  describe "GET lists" do
+    let(:client) { described_class.new('samdhoffman@gmail.com', 'todoable') }
+    let(:lists) { client.lists }
+
+    before do
+      VCR.insert_cassette 'lists', :record => :new_episodes
+    end
+
+    after do
+      VCR.eject_cassette
+    end
+
+    it "records the fixture" do
+      lists
+    end
+
+    it "response must have lists as an attribute" do
+      expect(lists).to include("lists")
+    end
+
+    it "must return an array of lists" do
+      expect(lists["lists"]).to be_instance_of(Array)
+    end
+
+    it "must have a lists method" do
+      expect(client).to respond_to(:lists)
+    end
+  end
   
 end
